76e42318505a675b7032cec0176618ccc3546a1e
Victor Andrade de Oliveira <victoroliveira.eng@gmail.com>
2019-03-03 21:43:52 +0100

    Changed translator package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { TranslatorModule } from 'angular-translator';
+import { HttpModule, Http } from '@angular/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: Http) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    TranslatorModule.forRoot()
+    HttpModule,
+    TranslateModule.forRoot({
+      useDefaultLang: true,
+      loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [Http]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
