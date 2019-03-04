af56c3e0201d9af48fbd54a512d0e738306cb84d
Andrade de Oliveira <vrai@gft.com>
2019-03-04 17:19:30 +0100

    First version of widget data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy }  from '@angular/core';
+import { forkJoin }           from 'rxjs/observable/forkJoin';
+
+import { DashboardService }   from './dashboard.service';
+import { Widget }             from '../interfaces/widget.interface';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  providers: [DashboardService]
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
+  widgetsOptions = ['bots', 'credentials', 'credit_cards', 'crime_servers', 'malware'];
+  widgetsList: Array<Widget> = [];
+  widgetSubscriptor: Subscription;
 
-  constructor() { }
+  constructor(private service: DashboardService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getWidgets();
+    console.log(this.widgetsList);
   }
 
+  ngOnDestroy(): void {
+    // Called once, before the instance is destroyed.
+    // Add 'implements OnDestroy' to the class.
+    this.widgetSubscriptor.unsubscribe();
+  }
+
+  getWidgets() {
+    const widgetObservables = this.createRequestsArray();
+
+    this.widgetSubscriptor = forkJoin(widgetObservables).subscribe((data) => {
+      for (const widget of data) {
+        console.log(data);
+        this.addWidget(widget);
+      }
+    });
+
+  }
+
+  createRequestsArray() {
+    const requests = [];
+    for (const option of this.widgetsOptions) {
+      requests.push(this.service.getWidgetData(option).map((res => res)));
+    }
+    return requests;
+  }
+
+
+  addWidget(data: any) {
+    this.widgetsList.push(data.data[0]);
+  }
 }
