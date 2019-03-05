16f9db48747ba91048f3843f249bbabd4d14c07a
Andrade de Oliveira <vrai@gft.com>
2019-03-05 17:37:34 +0100

    Refact dashboard page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,56 +1,31 @@
-import { Component, OnInit, OnDestroy }  from '@angular/core';
-import { forkJoin }           from 'rxjs/observable/forkJoin';
+import { Component, OnInit }  from '@angular/core';
 
-import { DashboardService }   from './dashboard.service';
-import { Widget }             from '../interfaces/widget.interface';
-import { Subscription } from 'rxjs/Subscription';
+import { WidgetList }         from '../widget/widget.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
-  providers: [DashboardService]
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit, OnDestroy {
-  widgetsOptions = ['bots', 'credentials', 'credit_cards', 'crime_servers', 'malware'];
-  widgetsList: Array<Widget> = [];
-  widgetSubscriptor: Subscription;
+export class DashboardComponent implements OnInit {
+  widgets: WidgetList = { list: [] };
 
-  constructor(private service: DashboardService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getWidgets();
-    console.log(this.widgetsList);
+    this.initWidgets();
   }
 
-  ngOnDestroy(): void {
-    // Called once, before the instance is destroyed.
-    // Add 'implements OnDestroy' to the class.
-    this.widgetSubscriptor.unsubscribe();
-  }
-
-  getWidgets() {
-    const widgetObservables = this.createRequestsArray();
-
-    this.widgetSubscriptor = forkJoin(widgetObservables).subscribe((data) => {
-      for (const widget of data) {
-        console.log(data);
-        this.addWidget(widget);
-      }
-    });
-
-  }
-
-  createRequestsArray() {
-    const requests = [];
-    for (const option of this.widgetsOptions) {
-      requests.push(this.service.getWidgetData(option).map((res => res)));
-    }
-    return requests;
-  }
-
-
-  addWidget(data: any) {
-    this.widgetsList.push(data.data[0]);
+  /**
+   * Initialize Widgets data
+   */
+  initWidgets(): void {
+    this.widgets.list = [
+      {title: 'BOTS', url: 'bots'},
+      {title: 'CREDENTIALS', url: 'credentials'},
+      {title: 'CREDIT CARDS', url: 'credit_cards'},
+      {title: 'CRIMESERVERS', url: 'crime_servers'},
+      {title: 'MALWARE', url: 'malware'}
+    ];
   }
 }
