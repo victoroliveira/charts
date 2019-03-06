000d1b3681fa5eafee885bc4aba27f66a94629bf
Andrade de Oliveira <vrai@gft.com>
2019-03-06 15:50:36 +0100

    Widget service unit tests

--- src/app/widget/widget.service.spec.ts
+++ src/app/widget/widget.service.spec.ts
@@ -1,15 +1,39 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { TestBed }                                        from '@angular/core/testing';
 
-import { WidgetService } from './widget.service';
+import { WidgetService }                                  from './widget.service';
 
 describe('WidgetService', () => {
+  let httpTestingController: HttpTestingController;
+  let service: WidgetService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [WidgetService]
     });
+
+    service = TestBed.get(WidgetService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
-  it('should be created', inject([WidgetService], (service: WidgetService) => {
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it('should call http get', () => {
+    const dummyResponse = {};
+    const widget = 'bots';
+    service.getWidgetData(widget).subscribe((response) => {
+      expect(response).toEqual(dummyResponse);
+    });
+    const URL = `../../assets/json/dashboard/widgets/${widget}_count.json`;
+    const req = httpTestingController.expectOne(URL);
+    expect(req.request.method).toEqual('GET');
+    req.flush(dummyResponse);
+  });
 });
