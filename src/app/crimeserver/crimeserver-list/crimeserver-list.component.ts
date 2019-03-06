0d17afdccdcb3b2ba31897814fdc71e75824a4fb
Andrade de Oliveira <vrai@gft.com>
2019-03-06 17:24:01 +0100

    Crimeserver list first layout

--- src/app/crimeserver/crimeserver-list/crimeserver-list.component.ts
+++ src/app/crimeserver/crimeserver-list/crimeserver-list.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy }    from '@angular/core';
+import { CrimeserverService }   from '../crimeserver.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-crimeserver-list',
   templateUrl: './crimeserver-list.component.html',
-  styleUrls: ['./crimeserver-list.component.scss']
+  styleUrls: ['./crimeserver-list.component.scss'],
+  providers: [CrimeserverService]
 })
-export class CrimeserverListComponent implements OnInit {
+export class CrimeserverListComponent implements OnInit, OnDestroy {
   count: number;
-  constructor() { }
+  data: any;
+  metadata: any;
+  listSubscription: Subscription;
+
+  constructor(private service: CrimeserverService) { }
 
   ngOnInit() {
-    this.count = 3;
+    this.getData();
+  }
+
+  ngOnDestroy() {
+    this.listSubscription.unsubscribe();
+  }
+
+  getData() {
+    this.listSubscription = this.service.getCrimeserverData().subscribe((response) => {
+      this.setData(response.data);
+      this.setMetadata(response.meta);
+    }, (error) => {
+      throw {message: error};
+    });
+  }
+
+  setData(data: any) {
+    this.data = data;
+  }
+
+  setMetadata(meta: any) {
+    this.metadata = meta;
+    this.updateCount(this.metadata.pagination.count);
+  }
+
+  updateCount(count: number) {
+    this.count = count;
   }
 
 }
